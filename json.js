4e786d8e0d2e4b278a1ee5de579e07f6eb13240e
surferzer <mafernandazr@gmail.com>
2019-10-21 14:27:45 -0500

    se agrega readme y base de datos falsa

---
+++ json.js
@@ -0,0 +1,99 @@
+"bracket" [
+
+    "competitors" [{
+            "id": "1",
+            "name": "Juan Pablo Benítez Rodríguez",
+            "age": "25 años",
+            "weight": "-61 kgs",
+            "years of trainning": "2 years",
+            "category": "male paja",
+            "notes": "lesión en rodilla derecha, qx hace 2 años",
+            "photo": "https://scontent.fgdl3-1.fna.fbcdn.net/v/t1.0-9/49697075_2008398092575906_4042235571874037760_n.jpg?_nc_cat=105&_nc_oc=AQmc7NEhRt0G9Uhd7AGo1eTqyY4ubeZWYRRuNo7BIXbUD-hKkvL7_FXZeN8E7Fwxhx4&_nc_ht=scontent.fgdl3-1.fna&oh=4a026a71a2ba45609748d880b5bd2418&oe=5E266A8C"
+        },
+        {
+            "id": "2",
+            "name": "Cristian Rosales Hernández",
+            "age": "26 años",
+            "weight": "-61 kgs",
+            "years of trainning": "5 years",
+            "category": "male paja",
+            "notes": "lesión en hombro derecho",
+            "photo": "https://scontent.fgdl3-1.fna.fbcdn.net/v/t1.0-9/59971679_2249699265120374_6967046757863653376_n.jpg?_nc_cat=103&_nc_oc=AQkf1fhuJErLTBe4pd5J5q23luFvxtGnmkFG0ZH3YHundk2Awa8UL8xsWnxYvOFYm8k&_nc_ht=scontent.fgdl3-1.fna&oh=4ce7c566721ade66360f26f7c2e572be&oe=5E168DB5"
+        },
+        {
+            "id": "3",
+            "name": "Eduardo Vázquez Díaz",
+            "age": "25 años",
+            "weight": "-61 kgs",
+            "years of trainning": "3 years",
+            "category": "male paja",
+            "notes": "subcampeón nacional 2019",
+            "photo": "https://scontent.fgdl3-1.fna.fbcdn.net/v/t1.0-9/65414708_708430499609164_28290034750717952_n.jpg?_nc_cat=101&_nc_oc=AQl_XqM4K1_dMiVLbemfY89noFcjabESt7V-1c-1TC4kRtp0nFNXlQc-YQi1HKwCV3Q&_nc_ht=scontent.fgdl3-1.fna&oh=425b2c69b1161669b36a3df0cd3cda85&oe=5E5D9E19"
+        },
+        {
+            "id": "4",
+            "name": "Dioney Zamora",
+            "age": "28 años",
+            "weight": "-61 kgs",
+            "years of trainning": "6 years",
+            "category": "male paja",
+            "notes": "intermitente en entrenamientos",
+            "photo": "https://scontent.fgdl3-1.fna.fbcdn.net/v/t1.0-9/44922897_2039694479626901_8573837471186419712_o.jpg?_nc_cat=110&_nc_oc=AQlUOtKyEOSnPLR-e_5SzGNtl_PcXwHUc-ELleIPuUbURtma7FWvpIwoLHh1yDkp3YM&_nc_ht=scontent.fgdl3-1.fna&oh=a98a9d6f946dfa84fc3d00529ddbd4c2&oe=5E651494"
+        },
+        {
+            "id": "5",
+            "name": "Luis Felipe Dominguez Ruiz",
+            "age": "29 años",
+            "weight": "-61 kgs",
+            "years of trainning": "6 years",
+            "category": "male paja",
+            "notes": "background de lucha olimpica por 3 años",
+            "photo": "https://scontent.fgdl3-1.fna.fbcdn.net/v/t1.0-9/12931256_1317064868310130_6853436337730238865_n.jpg?_nc_cat=101&_nc_oc=AQnyYY490oKNgTMSUxYTnQCw2DJ7n9Ka1tsHoVTYY4daGDioZoacN6SfwgtnKKf8F3Y&_nc_ht=scontent.fgdl3-1.fna&oh=fccaeb0c656ffecea4ade0c001a0051f&oe=5E27DEF3"
+        },
+        {
+            "id": "6",
+            "name": "Eduardo Murillo Cienfuegos",
+            "age": "24 años",
+            "weight": "-61 kgs",
+            "years of trainning": "4 years",
+            "category": "male paja",
+            "notes": "lesión en rodilla izquierda, Campeón Nacional 2019",
+            "photo": "https://scontent.fgdl3-1.fna.fbcdn.net/v/t1.0-9/43141040_2035872349807432_3348901049980157952_n.jpg?_nc_cat=110&_nc_oc=AQmWjKwsou99rYw9Ap__1Dp5rudti9sglzlBSRt9SspHOjg67mMwILgC_TON0hJKBCg&_nc_ht=scontent.fgdl3-1.fna&oh=dcfa3c726ee389f0bcea68a13ffbef05&oe=5E2CD593"
+        },
+        {
+            "id": "7",
+            "name": "Juan Jesús Calderón Pérez",
+            "age": "24 años",
+            "weight": "-61 kgs",
+            "years of trainning": "4 years",
+            "category": "male paja",
+            "notes": "debut hace 2 años en muay thai",
+            "photo": "https://scontent.fgdl3-1.fna.fbcdn.net/v/t1.0-9/44088264_1731065020331706_2166631784731639808_n.jpg?_nc_cat=104&_nc_oc=AQk8SCJ5UJ_CTL-C0pYxtA8t_o_iNn2s2sldwjtUp3K1x3urADWUElpzPK9oglos1mA&_nc_ht=scontent.fgdl3-1.fna&oh=4752890ad38c36b92298cdb9985ee951&oe=5E1DCAA4"
+        },
+        {
+            "id": "8",
+            "name": "Omar Galindo",
+            "age": "27 años",
+            "weight": "-61 kgs",
+            "years of trainning": "8 years",
+            "category": "male paja",
+            "notes": "profesional desde hace 4 años",
+            "photo": "https://scontent.fgdl3-1.fna.fbcdn.net/v/t1.0-9/57319790_265538317657221_3262997559416717312_n.jpg?_nc_cat=106&_nc_oc=AQmKq0W1yonUhRzFFu5jTqCTBRbO4uD1FEZylwECda2bkhIDf90fh_WSAQ0ayG9PCag&_nc_ht=scontent.fgdl3-1.fna&oh=7c871792e6ac5fdbe4a7171eff63b661&oe=5E1822BC"
+        },
+        {
+            "id": "9",
+            "name": "Ricardo López Nári",
+            "age": "27 años",
+            "weight": "-61 kgs",
+            "years of trainning": "8 years",
+            "category": "male paja",
+            "notes": "lesión en rodilla derecha persistente",
+            "photo": "https://scontent.fgdl3-1.fna.fbcdn.net/v/t1.0-9/10336585_10203100290241819_2607255864762522724_n.jpg?_nc_cat=105&_nc_oc=AQlezKvoLDoKjVrCp3_IplYvBY26QFy8QHoF4zn9NbDujzPhQbcaiMYuniG4Y4SvQzE&_nc_ht=scontent.fgdl3-1.fna&oh=b3b691568c830ac2171cf593836ff53e&oe=5E282A1D"
+        }
+
+
+    ]
+
+
+
+]
