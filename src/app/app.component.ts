7df38a5986e04682d6e9b7be9cc548ccb560a672
surferzer <mafernandazr@gmail.com>
2019-10-29 20:10:24 -0600

    funcionando bien titulo, bracket y muestra un peleador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ɵCompiler_compileModuleAndAllComponentsAsync__POST_R3__ } from '@angular/core';
 import { Url } from 'url';
 import { HtmlAstPath } from '@angular/compiler';
 import figthersList from '../assets/figthersList.json';
 import { BienvenidaComponent } from './bienvenida/bienvenida.component';
+
 
 
 
@@ -23,12 +24,12 @@ export class AppComponent {
 
   randomThis() {
     let aleatorio = [];
-   // let versusAleatorio=[];
+    // let versusAleatorio=[];
 
 
     for (let i = 0; i < this.fighters.length; i++) {
       aleatorio.push(Math.floor(Math.random() * (this.fighters.length - 1)));
-     // versusAleatorio.push(Math.floor(Math.random() * (this.fighters.length - 1)));
+      // versusAleatorio.push(Math.floor(Math.random() * (this.fighters.length - 1)));
 
       let removed = this.fighters.splice(aleatorio[0], 1)
       this.selectedFighters.push(removed[0].name);
@@ -37,57 +38,57 @@ export class AppComponent {
 
       console.log(resultado)
       this.newR.push(resultado)
-    
+
       //return console.log(this.newR);  
       return resultado;
-        
+
     }
-    
+
   }
 
-  round2:string[]=[];
+  round2: string[] = [];
 
-winner(){
-  this.round2.push(this.selectedFighters[0]);
- }
+  winner() {
+    this.round2.push(this.selectedFighters[0]);
+  }
 
- winner1(){
-   this.round2.push(this.selectedFighters[1]);
- }
+  winner1() {
+    this.round2.push(this.selectedFighters[1]);
+  }
 
- winner2(){
-   this.round2.push(this.selectedFighters[2]);
- }
+  winner2() {
+    this.round2.push(this.selectedFighters[2]);
+  }
 
- winner3(){
-   this.round2.push(this.selectedFighters[3]);
- }
+  winner3() {
+    this.round2.push(this.selectedFighters[3]);
+  }
 
- winner4(){
-   this.round2.push(this.selectedFighters[4]);
- }
- 
- winner5(){
-   this.round2.push(this.selectedFighters[5]);
- }
+  winner4() {
+    this.round2.push(this.selectedFighters[4]);
+  }
 
- winner6(){
-   this.round2.push(this.selectedFighters[6]);
- }
- 
- winner7(){
-   this.round2.push(this.selectedFighters[7]);
- }
+  winner5() {
+    this.round2.push(this.selectedFighters[5]);
+  }
 
- winner8(){
-   this.round2.push(this.selectedFighters[8]);
- }
+  winner6() {
+    this.round2.push(this.selectedFighters[6]);
+  }
 
+  winner7() {
+    this.round2.push(this.selectedFighters[7]);
+  }
 
- title: string[] = [];
+  winner8() {
+    this.round2.push(this.selectedFighters[8]);
+  }
 
 
- myTitle() {
+  title: string[] = [];
+
+
+  myTitle() {
     return this.title.push(this.round2[0]);
   }
 
@@ -103,22 +104,26 @@ winner(){
     return this.title.push(this.round2[3]);
   }
 
-  almostChampion: string[]=[];
+  almostChampion: string[] = [];
 
-  aChamp(){
+  aChamp() {
     return this.almostChampion.push(this.title[0]);
   }
 
-  aChamp1(){
+  aChamp1() {
     return this.almostChampion.push(this.title[1]);
   }
 
+
+  getPhoto() {
+
+  }
 
   id: string;
   name: string;
   age: string;
   weight: string;
-  yearsOfTrainning: number;
+
   category: string;
   notes: string;
   photo: string;
@@ -126,20 +131,17 @@ winner(){
 
 
 
-
   constructor() {
 
-    this.id = "1";
-    this.name = "Juan Pablo Benítez Rodríguez"
-    this.age = "25 años"
-    this.weight = "-61kgs"
-    this.yearsOfTrainning = 2;
-    this.category = "male paja"
-    this.notes = "lesion en rodilla derecha, qx hace 2 años"
-    this.photo = "https://scontent.fgdl3-1.fna.fbcdn.net/v/t1.0-9/49697075_2008398092575906_4042235571874037760_n.jpg?_nc_cat=105&_nc_oc=AQmc7NEhRt0G9Uhd7AGo1eTqyY4ubeZWYRRuNo7BIXbUD-hKkvL7_FXZeN8E7Fwxhx4&_nc_ht=scontent.fgdl3-1.fna&oh=4a026a71a2ba45609748d880b5bd2418&oe=5E266A8C"
+    this.id = this.fighters[0].id
+    this.name = this.fighters[0].name
+    this.age = this.fighters[0].age
+    this.weight = this.fighters[0].weight
+
+    this.category = this.fighters[0].category
+    this.notes = this.fighters[0].notes
+    this.photo = this.fighters[0].photo
+
 
   }
-
-
-
 }
